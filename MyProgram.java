5b554a4912b8375e12aa4cabfd7d7ffd6945c75a
jayanth reddy <72425466+Jayanth-redd@users.noreply.github.com>
2023-10-29 15:26:27 +0530

    Add files via upload

---
+++ MyProgram.java
@@ -0,0 +1,17 @@
+package com.tech.pro;
+
+public class MyProgram {
+
+	public static void main(String[] args) {
+		Student s=new Student();
+		//s.creating();
+		//s.inserting("jayanth", "java", "jayanth@gmail.com");
+		//s.inserting("hemanth", "python", "hemanth12@gmail.com");
+		//s.inserting("naveen", "datascience", "naveen45@gmail.com");
+		//s.update(4, "naveen200@gmail.com", "machineLearning");
+		//s.delete(3);
+		s.read();
+
+	}
+
+}
